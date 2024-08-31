358667638083fad830221f93c41d98451296c3fb
Elias Souza <elias.ssouza@hotmail.com>
2024-08-30 23:58:24 -0300

    criando navegação para detalhe do produto

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-
   final Product product;
-  const ProductItem({Key? key, required this.product,}) : super (key: key);
-
-  
+  const ProductItem({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +15,34 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
-            title: Text(product.title,
-            textAlign: TextAlign.center),
-            backgroundColor: Colors.black87,
-            leading: IconButton(onPressed: () {}, 
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
             icon: const Icon(Icons.favorite),
             color: Theme.of(context).colorScheme.secondary,
-            ),
-            trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
-            ),
           ),
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
+        ),
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.productDetail,
+              arguments: product,
+            );
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/pages/product_detail_page.dart';
 import 'package:shop/models/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Minha Loja',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple, secondary: Colors.deepOrange),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.purple,
+          secondary: Colors.deepOrange,
+        ),
         fontFamily: 'Lato',
-        useMaterial3: true,
       ),
       home: ProductsOverviewPage(),
+      routes: {
+        AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }

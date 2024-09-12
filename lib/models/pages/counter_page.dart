b80f0e86e5b69f22ef5064e4ebf540dd4800e04a
Elias Souza <31075618+elias-ssouza@users.noreply.github.com>
2024-09-11 23:06:07 -0300

    criação da counter_page

---
+++ lib/models/pages/counter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Exemplo Contador"),
+      ),
+    );
+  }
+}

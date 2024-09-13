a1ee9f7873a972ee91250945c220e1431de05389
Elias Souza <31075618+elias-ssouza@users.noreply.github.com>
2024-09-12 23:20:55 -0300

    criação do counter provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/pages/counter_page.dart';
 import 'package:shop/models/pages/product_detail_page.dart';
 import 'package:shop/models/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       ),
       home: ProductsOverviewPage(),
       routes: {
-        AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        AppRoutes.productDetail: (ctx) => const CounterPage(),
       },
       debugShowCheckedModeBanner: false,
     );
